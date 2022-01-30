a63fa923ab983ce7eaa1ea9f4aa17907d2245d01
yuxiangw <yuxian660@163.com>
2022-01-30 23:28:06 +0800

    add SFC

---
+++ code/SFC/hello/hello.vue
@@ -0,0 +1,23 @@
+<template>
+   <div id="msg">
+      {{ message }}
+   </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         message: "Hello SFC"
+      }
+   }
+}
+</script>
+
+<style>
+#msg {
+   font-size: 18px;
+   font-family: "Roboto", sans-serif;
+   color: blue;
+}
+</style>
