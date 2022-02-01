df64a556a4f31e51be4f7ebbd609c5cdabfbc676
yuxiangw <yuxian660@163.com>
2022-02-02 00:39:01 +0800

    update icon test

--- code/elementUI/hello/index.js
+++ code/elementUI/hello/index.js
@@ -8,4 +8,7 @@ const HelloVueApp = {
 
 const app = Vue.createApp(HelloVueApp);
 app.use(ElementPlus);
+// icons-vue is under development: https://element-plus.gitee.io/zh-CN/component/icon.html
+app.component('edit', ElementPlusIconsVue.Edit)
+app.component('share', ElementPlusIconsVue.Share)
 const vm = app.mount('#app')
