4b3e611742202b780c6af361ebcf30201fb58d8f
yuxiangw <yuxian660@163.com>
2022-02-04 22:11:57 +0800

    update doc

--- code/demo/single_page/js/home/index.js
+++ code/demo/single_page/js/home/index.js
@@ -1,17 +1,29 @@
-import vCommon from "../common/index.js"
-
 const vHome = {
-   components : {
-      'icommon': vCommon
+   data() {
+      return {
+         textarea: ''
+      }
    },
    template: `
       <div class='vhome'>
          <el-row :gutter="20">
             <el-col class="grid-content bg-purple" :span="12">
-               <icommon />
+               <el-input
+                  v-model="textarea"
+                  :rows="19"
+                  type="textarea"
+                  placeholder="Please input"
+               />
+               <el-button>Submit</el-button>
             </el-col>
             <el-col class="grid-content bg-purple-dark" :span="12">
-               <icommon />
+               <el-input
+                  v-model="textarea"
+                  :rows="19"
+                  type="textarea"
+                  placeholder=""
+                  disabled
+               />
             </el-col>
          </el-row>
       </div>
